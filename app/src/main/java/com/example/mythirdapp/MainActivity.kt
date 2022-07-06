df09a0fceeb6a9f1d0fa3ba088028f7e3cceadef
chisato30 <moterunit@gmail.com>
2022-07-06 17:09:56 +0900

    HelloWorld→This　is　sample

--- app/src/main/java/com/example/mythirdapp/MainActivity.kt
+++ app/src/main/java/com/example/mythirdapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mythirdapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text1 = findViewById<TextView>(R.id.text1)
+        text1.text = "This is sample"
     }
 }
