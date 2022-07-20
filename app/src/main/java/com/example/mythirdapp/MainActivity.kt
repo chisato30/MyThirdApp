294e30617c048c5a4d1bb7c0bcefdb1fec27abc0
chisato30 <moterunit@gmail.com>
2022-07-20 19:42:08 +0900

    ダイアログで新規入力実装

--- app/src/main/java/com/example/mythirdapp/MainActivity.kt
+++ app/src/main/java/com/example/mythirdapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mythirdapp
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,26 +13,38 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val myViewModel: MyViewModel = ViewModelProvider(this)
             .get(MyViewModel::class.java)
         setContentView(R.layout.activity_main)
 
+        text1.text = myViewModel.allByText()
         button1.setOnClickListener {
-            val nm =frm_name.text
-            val ml =frm_mail.text
-            val ag =frm_age.text
-            myViewModel.add(nm.toString(), ml.toString(), ag.toString().toInt())
-            text1.text = updateData(myViewModel)
+            val vw = layoutInflater.inflate(R.layout.mydialog, null)
+            val dlg_name = vw.findViewById<EditText>(R.id.dlg_name)
+            val dlg_mail = vw.findViewById<EditText>(R.id.dlg_mail)
+            val dlg_age = vw.findViewById<EditText>(R.id.dlg_age)
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Alert")
+            builder.setView(vw)
+                .setPositiveButton("OK",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        myViewModel.add(dlg_name.text.toString(),
+                            dlg_mail.text.toString(),
+                            dlg_age.text.toString().toInt())
+                        text1.text = myViewModel.allByText()
+                    })
+                .setNegativeButton("Cancel",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        Toast.makeText(this, "Canceled.",
+                            Toast.LENGTH_SHORT).show()
+                    })
+
+            builder.create().show()
         }
     }
-            fun updateData(vm: MyViewModel): String {
-                val data = vm.getAll()
-                var people = ""
-                for (item in data) {
-                    people += item.to_s()
-                    people += "\n"
-                }
-
-                return people
-    }
 }
+
+
+
--- app/src/main/java/com/example/mythirdapp/MyViewModel.kt
+++ app/src/main/java/com/example/mythirdapp/MyViewModel.kt
@@ -1,28 +1,108 @@
 package com.example.mythirdapp
 
-//import android.app.Person
+import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
-    private val data:MutableList<Person> = mutableListOf(
-        Person("Taro","taro@yamada", 36) ,
+    var data: MutableList<Person> = mutableListOf(
+        Person("Taro", "taro@yamada", 36),
         Person("Hanako", "hanako@flower", 25),
         Person("Sachiko", "sachiko@happy", 14)
     )
 
-    fun getAll():List<Person> = data
+    fun allByText(): String {
+        var res = ""
+        for (item in data) {
+            res += item.to_s()
+            res += "\n"
+        }
+        return res
+    }
 
-    fun getById(id:Int):Person = data[id]
+    fun getById(id: Int): Person = data[id]
 
-    fun add(name:String, mail:String, age:Int){
+    fun add(name: String, mail: String, age: Int) {
         data.add(Person(name, mail, age))
     }
 }
 
-class Person(name:String, mail:String, age:Int){
+class Person(name:String, mail:String, age:Int) {
     var name:String = name
     var mail:String = mail
     var age:Int = age
 
-    fun to_s(): Any? = "$name ($mail, $age)"
+    fun age_s():String = age.toString()
+
+    fun to_s(): Any?= "$name ($mail, $age)"
 }
+
+
+
+//    MutableLiveData<MutableList<Person>>()
+//    }
+//
+//    val name:MutableLiveData<String> by lazy {
+//        MutableLiveData<String>()
+//    }
+//
+//    val mail:MutableLiveData<String> by lazy {
+//        MutableLiveData<String>()
+//    }
+//
+//    val age:MutableLiveData<String> by lazy {
+//        MutableLiveData<String>()
+//    }
+//
+//    val allText:MutableLiveData<String> by lazy {
+//        MutableLiveData<String>()
+//    }
+//    val nameObserver = Observer<String> {newName ->
+//        frm_name.setText(newName, TextView.BufferType.NORMAL)
+//            }
+//    myViewModel.name.observe(this, nameObserver)
+//
+////    val allText:MutableLiveData<String> by lazy {
+////        MutableLiveData<String>()
+////    }
+//
+//    fun allByText(): String {
+//        var res = ""
+//        for (item in data) {
+//            res += item.to_s()
+//            res += "\n"
+//        }
+//        return res
+//    }
+//
+//    fun getById(id:Int): Person = data[id]
+//
+//    fun add(name:String, mail:String, age:Int) {
+//        var name:String = name
+//        var mail:String = mail
+//        var age:Int = age
+//    }
+//
+//    init {
+//        data.value = mutableListOf(
+//            Person("Taro", "taro@yamada", 36),
+//            Person("Hanako", "hanako@flower", 25),
+//            Person("Sachiko", "sachiko@happy", 14)
+//        )
+//      allText.value = allByText()
+//        name.value = "name"
+//        mail.value = "mail@address"
+//        age.value = "0"
+//    }
+//}
+//
+//class Person(name:String, mail:String, age:Int) {
+//    var name:String = name
+//    var mail:String = mail
+//    var age:Int = age
+//
+//    fun age_s():String = age.toString()
+//
+//    fun to_s(): Any?= "$name ($mail, $age)"
+//}
